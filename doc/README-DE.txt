3966aa52ed14c63abae0fd354fe487b448d7d104
saasrepo <saasrepo@saasrepo.symmetrics.de>
2010-06-02 19:42:43 +0200

    Initial import

---
+++ doc/README-DE.txt
@@ -0,0 +1,32 @@
+* DOCUMENTATION
+
+** INSTALLATION
+Extrahieren Sie den Inhalt dieses Archivs in Ihr Magento Verzeichnis.
+
+** USAGE
+
+** FUNCTIONALITY
+*** A:
+*** B:
+*** C:
+
+** TECHNICAL
+
+** PROBLEMS
+
+* TESTCASES
+
+** BASIC
+*** A:
+*** B:
+*** C:
+
+** CATCHABLE
+*** A:
+*** B:
+*** C:
+
+** STRESS
+*** A:
+*** B:
+*** C:
---
+++ info.py
@@ -0,0 +1,70 @@
+# encoding: utf-8
+
+
+# =============================================================================
+# package info
+# =============================================================================
+NAME = 'symmetrics_module_buyerprotect'
+
+TAGS = ()
+
+LICENSE = 'AFL 3.0'
+
+HOMEPAGE = 'http://www.symmetrics.de'
+
+INSTALL_PATH = ''
+
+
+# =============================================================================
+# responsibilities
+# =============================================================================
+TEAM_LEADER = {
+    '[firstname] [lastname]': '[email]',
+}
+
+MAINTAINER = {
+    '[firstname] [lastname]': '[email]',
+}
+
+AUTHORS = {
+    '[firstname] [lastname]': '[email]',
+}
+
+# =============================================================================
+# additional infos
+# =============================================================================
+INFO = '[short description, max 60 chars]'
+
+SUMMARY = '''
+    [long description
+    with multiple lines]
+'''
+
+NOTES = '''
+    [special notes, restrictions, bugs etc.]
+'''
+
+# =============================================================================
+# relations
+# =============================================================================
+REQUIRES = [
+    # {'magento': '*', 'magento_enterprise': '*'},
+]
+
+EXCLUDES = {
+}
+
+VIRTUAL = {
+}
+
+DEPENDS_ON_FILES = (
+    'app/code/core/.../1.php',
+    'app/code/core/.../2.php',
+    'app/code/core/.../3.php',
+)
+
+PEAR_KEY = ''
+
+COMPATIBLE_WITH = {
+    # 'magento': ['1.3.2', '1.3.2.1', ...],
+}
