35347de51b82fb26d04de0ca7d045ad6d6dca676
Ngoc Anh Doan <nd@symmetrics.de>
2010-06-09 10:52:49 +0200

    Testaufruf der Klasse Symmetrics_Buyerprotect_Model_Service_Soap_Data.

--- build/app/code/community/Symmetrics/Buyerprotect/Model/Service/Soap.php
+++ build/app/code/community/Symmetrics/Buyerprotect/Model/Service/Soap.php
@@ -36,53 +36,6 @@
 class Symmetrics_Buyerprotect_Model_Service_Soap
 {
     /**
-     * Keys of of data:
-     *
-     * returnValue: return_value
-     * tsId: ts_id
-     * tsProductId: ts_product_id
-     * amount: amount
-     * currency: currency
-     * paymentType: payment_type
-     * buyerEmail: buyer_email
-     * shopCustomerID: shop_customer_id
-     * shopOrderID: shop_order_id
-     * orderDate: order_date
-     * wsUser: ws_user
-     * wsPassword: ws_password
-     *
-     * @param Mage_Sales_Model_Order $orderObject Order object
-     * 
-     * @return Varien_Object
-     */
-    protected function _initTsSoapDataObject(Mage_Sales_Model_Order $orderObject)
-    {
-        $tsSoapDataObject = new Varien_Object();
-
-        $tsStoreConfigPaths = array(
-            'is_active' => Symmetrics_Buyerprotect_Helper_Data::XML_PATH_TS_BUYERPROTECT_IS_ACTIVE,
-            'ts_id' => Symmetrics_Buyerprotect_Helper_Data::XML_PATH_TS_BUYERPROTECT_TS_ID,
-            'ws_user' => Symmetrics_Buyerprotect_Helper_Data::XML_PATH_TS_BUYERPROTECT_TS_USER,
-            'ws_password' => Symmetrics_Buyerprotect_Helper_Data::XML_PATH_TS_BUYERPROTECT_TS_PASSWORD,
-            'wsdl_url' => Symmetrics_Buyerprotect_Helper_Data::XML_PATH_TS_BUYERPROTECT_TS_WSDL_URL
-        );
-
-        $tsSoapData = array(
-            'is_active' => Mage::getStoreConfig($tsStoreConfigPaths['is_active']),
-            'ts_id' => Mage::getStoreConfig($tsStoreConfigPaths['ts_id']),
-            'ws_user' => Mage::getStoreConfig($tsStoreConfigPaths['ws_user']),
-            'ws_password' => Mage::getStoreConfig($tsStoreConfigPaths['ws_password']),
-            'wsdl_url' => Mage::getStoreConfig($tsStoreConfigPaths['wsdl_url']),
-            'buyer_email' => $orderObject->getCustomerEmail(),
-            'amount' => (double) $orderObject->getGrandTotal(),
-            'shop_order_id' => $orderObject->getRealOrderId(),
-            'order_date' => str_replace(' ', 'T', $order->getCreatedAt()),
-        );
-
-        return $tsSoapDataObject->setData($tsSoapData);
-    }
-
-    /**
      * make a protection request to the TrustedShops Soap Api
      *
      * @param Mage_Sales_Model_Order $order order to make a Reqest from
@@ -101,9 +54,10 @@ class Symmetrics_Buyerprotect_Model_Service_Soap
 
         if ($orderItemsCollection->count() >= 1) {
             $firstItem = $orderItemsCollection->getFirstItem();
-            $tsSoapDataObject = $this->_initTsSoapDataObject();
+            /* @var $tsSoapDataObject Symmetrics_Buyerprotect_Model_Service_Soap_Data */
+            $tsSoapDataObject = Mage::getModel('buyerprotect/service_soap_data', array($firstItem, $order));
 
-
+            $tsSoapData = $tsSoapDataObject->getTsSoapData();
             /** @todo make Soap Call */
         }
     }
