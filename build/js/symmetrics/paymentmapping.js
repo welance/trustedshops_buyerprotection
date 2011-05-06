b686763c79efa50145a1915a2a9f268feab2edba
Eric Reiche <er@symmetrics.de>
2011-05-06 12:35:42 +0200

    Removed payment mapping code.

--- build/js/symmetrics/paymentmapping.js
+++ build/js/symmetrics/paymentmapping.js
@@ -1,20 +1,3 @@
-document.observe('dom:loaded', function(){
-    var paypalNotice = $$('.paypal-payment-notice').first();
-    if (paypalNotice) {
-        var fieldTemplate = '';
-
-        $$('.main-col-inner #config_edit_form fieldset.config').each(function(sectionNode) {
-            var tableNode = sectionNode.select('table.form-list tbody');
-            var firstField = tableNode.select('tr').first();
-            var sectionId = sectionNode.identify();
-            var newField = fieldTemplate;
-            newField = newField.replace(/\%code\%/, sectionId);
-            newField = newField.replace(/\%options\%/, '');
-            if (firstField) {
-                firstField.insert({
-                    after: newField
-                });
-            }
-        })
-    }
-})
+/**
+ * @todo: implement
+ */
