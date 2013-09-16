d54323f4754c952db545fc03fafc81f578ac7c0d
Ngoc Anh Doan <ngoc-anh.doan@cgi.com>
2013-09-16 15:35:36 +0200

    Corrected info tags in doc header.

--- build/app/code/community/Symmetrics/Buyerprotect/Model/Observer.php
+++ build/app/code/community/Symmetrics/Buyerprotect/Model/Observer.php
@@ -18,7 +18,7 @@
  * @author    Torsten Walluhn <tw@symmetrics.de>
  * @author    Benjamin Klein <bk@symmetrics.de>
  * @author    Ngoc Anh Doan <ngoc-anh.doan@cgi.com>
- * @copyright 2010-2012 symmetrics - a CGI Group brand
+ * @copyright 2010-2013 symmetrics - a CGI Group brand
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
  */
@@ -34,7 +34,7 @@
  * @author    Benjamin Klein <bk@symmetrics.de>
  * @author    Andreas Timm <at@symmetrics.de>
  * @author    Ngoc Anh Doan <ngoc-anh.doan@cgi.com>
- * @copyright 2010-2012 symmetrics - a CGI Group brand
+ * @copyright 2010-2013 symmetrics - a CGI Group brand
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
  */
